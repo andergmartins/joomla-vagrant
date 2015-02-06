602dc866012c39c11c706360179888d473b1a866
Anderson Grudtner Martins <anderson@ostraining.com>
2015-02-06 10:56:33 -0200

    Adds phantomjs module to the manifest

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -231,3 +231,11 @@ class { 'scripts': }
 class { 'phpmanager': }
 
 class { 'docker': }
+
+class { 'phantomjs':
+    package_version => '1.9.7',
+    package_update => true,
+    install_dir => '/usr/local/bin',
+    source_dir => '/opt',
+    timeout => 300
+}
